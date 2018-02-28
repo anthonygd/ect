4be7197ced2ba8446bd306be9dca38d154e82485
anthony dean <anthony@safechain.io>
2018-02-27 21:35:17 -0500

    Dropped back to 0.4.18.  Added unit tests.

--- contracts/EthereumColumbusToken.sol
+++ contracts/EthereumColumbusToken.sol
@@ -1,6 +1,7 @@
-pragma solidity ^0.4.19;
+pragma solidity ^0.4.18;
 
 import "contracts/ERC20.sol";
+import "contracts/SafeMath.sol";
 
 contract EthereumColumbusToken is ERC20 {
 
@@ -9,34 +10,63 @@ contract EthereumColumbusToken is ERC20 {
     string public constant symbol = "ECT";
     uint8 public constant decimals = 18;
 
+    // Private data structures
+    mapping (address => uint) private balances;
+    mapping (address => mapping (address => uint)) private allowances;
+    uint private supply;
+    address contract_owner;
+
+    // Minimal setup required
+    function EthereumColumbusToken() public {
+      contract_owner = msg.sender;
+    }
+
     // Standard view functions
     function totalSupply() public constant returns (uint) {
-
+      return supply;
     }
 
     function balanceOf(address _tokenOwner) public constant returns (uint balance) {
-
+      return balances[_tokenOwner];
     }
 
     function allowance(address _tokenOwner, address _spender) public constant returns (uint remaining) {
-
+      return allowances[_tokenOwner][_spender];
     }
 
     // Standard update functions
     function transfer(address _to, uint _tokens) public returns (bool success) {
+      balances[msg.sender] = SafeMath.sub(balances[msg.sender], _tokens);
+      balances[_to] = SafeMath.add(balances[_to], _tokens);
 
+      Transfer(msg.sender, _to, _tokens);
+
+      return true;
     }
 
     function approve(address _spender, uint _tokens) public returns (bool success) {
+      allowances[msg.sender][_spender] = _tokens;
 
+      Approval(msg.sender, _spender, _tokens);
+
+      return true;
     }
 
     function transferFrom(address _from, address _to, uint _tokens) public returns (bool success) {
+      allowances[_from][_to] = SafeMath.sub(allowances[_from][_to], _tokens);
+      balances[_from] = SafeMath.sub(balances[_from], _tokens);
+      balances[_to] = SafeMath.add(balances[_to], _tokens);
 
+      Transfer(_from, _to, _tokens);
+
+      return true;
     }
 
     // Non-Standard creation function
     function mint(uint _tokens) public returns (bool success) {
-        
+      require(msg.sender == contract_owner);
+      balances[msg.sender] = SafeMath.add(balances[msg.sender], _tokens);
+      supply = SafeMath.add(supply, _tokens);
+      return true;
     }
 }
--- test/test_ect.js
+++ test/test_ect.js
@@ -2,16 +2,44 @@ const ECT = artifacts.require("EthereumColumbusToken.sol");
 
 contract('EthereumColumbusToken', function(accounts) {
 
-    var me = accounts[0]
-    var you = accounts[1]
+  var me = accounts[0]
+  var you = accounts[1]
+  beforeEach('setup contract for each test', async function() {
+    instance = await ECT.new()
+    assert.ok(me, 'primary account missing')
+    assert.ok(you, 'no secondary account')
+  })
 
-    beforeEach('setup contract for each test', async function() {
-        instance = await ECT.new()
-        assert.ok(me, 'primary account missing')
-        assert.ok(you, 'no secondary account')
-    })
+  it('should be constructable', async function() {
+    assert.equal(await instance.totalSupply.call(), 0)
+  })
 
-    it('should be constructable', async function() {
-        assert.equal(await instance.totalSupply.call(), 0)
-    })
+  it('should be able to mint tokens', async function() {
+    assert.ok(instance.mint.call(100))
+
+    await instance.mint(1500000000000000000)
+    assert.equal(await instance.totalSupply(), 1500000000000000000)
+  })
+
+  it('should be able to transfer tokens', async function() {
+    await instance.mint(1500000000000000000)
+
+    assert.ok(await instance.transfer.call(you, 100))
+    await instance.transfer(you, 100)
+
+    my_bal = await instance.balanceOf(me)
+
+    assert.equal(my_bal.toString(), '1499999999999999900') // CAREFUL- BigNumber
+    assert.equal(await instance.balanceOf(you), 100)
+  })
+
+  it('should not be able to transfer more than I have', async function() {
+    await instance.mint(100)
+    assert.equal(await instance.balanceOf(me), 100)
+
+    await instance.transfer(you, 150)
+
+    assert.equal(await instance.balanceOf(me), 100)
+    assert.equal(await instance.balanceOf(you), 0)
+  })
 })
